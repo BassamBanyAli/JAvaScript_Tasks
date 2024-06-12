1a42a9be6b8c463d17d7f66f344cc0d18ed9cfd1
BassamBanyAli <bassambanyali@gmail.com>
2024-06-12 02:20:03 -0700

    task 5 modified

--- task 5/index3.js
+++ task 5/index3.js
@@ -1,39 +1,37 @@
-document.getElementById("fname").addEventListener("change", function() {
+document.getElementById("fname").addEventListener("input", function() {
     NoNumber(this);
 });
 
 function NoNumber(obj) {
     const namePattern = /^[^\d]*$/;
-    if (!namePattern.test(obj.value)) {
+    if ((!namePattern.test(obj.value))&&obj.value!=="") {
         document.getElementById("fspan").innerHTML = "Numbers are not allowed";
-    } else {
+    } else{
         document.getElementById("fspan").innerHTML = "";
     }
 }
 
-document.getElementById("Bdate").addEventListener("change", function() {
+document.getElementById("Bdate").addEventListener("input", function() {
     Bdate(this);
 });
 
 function Bdate(obj) {
     const datePattern = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
-    if (!datePattern.test(obj.value)) {
+    if ((!datePattern.test(obj.value))&&obj.value!="") {
         document.getElementById("Bspan").innerHTML = "The format should be dd/mm/yyyy";
     } else {
         document.getElementById("Bspan").innerHTML = "";
     }
 }
 
-document.getElementById("email").addEventListener("change", function() {
+document.getElementById("email").addEventListener("input", function() {
     validateEmail(this);
 });
 
 function validateEmail(obj) {
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!emailPattern.test(obj.value)) {
+    if ((!emailPattern.test(obj.value))&& obj.value!=="") {
         document.getElementById("Espan").innerHTML = "The e-mail is not valid";
-    } else if (obj.value === "") {
-        document.getElementById("Espan").innerHTML = "The e-mail is required";
     } else {
         document.getElementById("Espan").innerHTML = "";
     }
